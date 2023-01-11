20b76812850bd5646ee2971c4f2d367db8c92ac1
dhivkk21 <dhivkk21@outlook.com>
2023-01-11 18:49:16 +0530

    kk

--- Dttl.Qr.Repository/Interface/ISearchService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Dttl.Qr.Model;
-
-namespace Dttl.Qr.Repository.Interface
-{
-    public interface ISearchService
-    {
-        public Task<List<FilterData>> GetSearchByFilter(SearchFilter searchFilter);
-    }
-}
